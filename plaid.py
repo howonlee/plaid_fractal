8ad6236910debb80fdf1e7601a974baae9182de7
Howon Lee <howon@howonlee.com>
2016-01-21 11:36:52 -0800

    add skg

--- plaid.py
+++ plaid.py
@@ -6,8 +6,9 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print "usage: python plaid.py <name of edgelist file>"
         print "edgelist files like from snap.stanford.edu"
+        print "big memory problems with the really really huge nets, tho"
     edgelist_file = sys.argv[1]
-    adjmat = np.zeros((1000, 1000))
+    adjmat = np.zeros((2000, 2000))
     line_num = 0
     with open(edgelist_file) as edgelist_file:
         for line in edgelist_file:
@@ -18,7 +19,8 @@ if __name__ == "__main__":
                 print line_num
             fst, snd = line.strip().split()
             fst, snd = int(fst), int(snd)
-            if fst < 1000 and snd < 1000:
+            if fst < 2000 and snd < 2000:
                 adjmat[fst, snd] = 1
-    plt.imshow(adjmat)
+    plt.imshow(adjmat, cmap='Greys')
+    plt.title("Plaid network")
     plt.show()
