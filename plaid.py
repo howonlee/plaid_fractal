b225cb22cb34be359cd87c95bf22b8a1c981ab6c
Howon Lee <howon@howonlee.com>
2016-01-21 11:27:58 -0800

    let's see that plaid

--- plaid.py
+++ plaid.py
@@ -1,5 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 if __name__ == "__main__":
-    pass
+    if len(sys.argv) != 2:
+        print "usage: python plaid.py <name of edgelist file>"
+        print "edgelist files like from snap.stanford.edu"
+    edgelist_file = sys.argv[1]
+    line_num = 0
+    with open(edgelist_file) as edgelist_file:
+        for line in edgelist_file:
+            line_num += 1
+            print line
+            if line_num > 1000:
+                break
